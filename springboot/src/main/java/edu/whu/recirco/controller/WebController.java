ef00eeb30b1bd75ef11b2125dc10d18075de96b6
邓奇 <2043688237@qq.com>
2024-10-13 09:27:07 +0800

    更新Java项目组织名

---
+++ springboot/src/main/java/edu/whu/recirco/controller/WebController.java
@@ -0,0 +1,73 @@
+package edu.whu.recirco.controller;
+
+import cn.hutool.core.util.ObjectUtil;
+import cn.hutool.core.util.StrUtil;
+import edu.whu.recirco.common.Result;
+import edu.whu.recirco.common.enums.ResultCodeEnum;
+import edu.whu.recirco.common.enums.RoleEnum;
+import edu.whu.recirco.entity.Account;
+import edu.whu.recirco.service.AdminService;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+/**
+ * 基础前端接口
+ */
+@RestController
+public class WebController {
+
+    @Resource
+    private AdminService adminService;
+
+    @GetMapping("/")
+    public Result hello() {
+        return Result.success("访问成功");
+    }
+
+    /**
+     * 登录
+     */
+    @PostMapping("/login")
+    public Result login(@RequestBody Account account) {
+        if (ObjectUtil.isEmpty(account.getUsername()) || ObjectUtil.isEmpty(account.getPassword())
+                || ObjectUtil.isEmpty(account.getRole())) {
+            return Result.error(ResultCodeEnum.PARAM_LOST_ERROR);
+        }
+        if (RoleEnum.ADMIN.name().equals(account.getRole())) {
+            account = adminService.login(account);
+        }
+        return Result.success(account);
+    }
+
+    /**
+     * 注册
+     */
+    @PostMapping("/register")
+    public Result register(@RequestBody Account account) {
+        if (StrUtil.isBlank(account.getUsername()) || StrUtil.isBlank(account.getPassword())
+                || ObjectUtil.isEmpty(account.getRole())) {
+            return Result.error(ResultCodeEnum.PARAM_LOST_ERROR);
+        }
+        if (RoleEnum.ADMIN.name().equals(account.getRole())) {
+            adminService.register(account);
+        }
+        return Result.success();
+    }
+
+    /**
+     * 修改密码
+     */
+    @PutMapping("/updatePassword")
+    public Result updatePassword(@RequestBody Account account) {
+        if (StrUtil.isBlank(account.getUsername()) || StrUtil.isBlank(account.getPassword())
+                || ObjectUtil.isEmpty(account.getNewPassword())) {
+            return Result.error(ResultCodeEnum.PARAM_LOST_ERROR);
+        }
+        if (RoleEnum.ADMIN.name().equals(account.getRole())) {
+            adminService.updatePassword(account);
+        }
+        return Result.success();
+    }
+
+}
---
+++ springboot/src/main/java/edu/whu/recirco/mapper/TypeMapper.java
@@ -0,0 +1,37 @@
+package edu.whu.recirco.mapper;
+
+import edu.whu.recirco.entity.Type;
+
+import java.util.List;
+
+/**
+ * 操作notice相关数据接口
+ */
+public interface TypeMapper {
+
+    /**
+     * 新增
+     */
+    int insert(Type type);
+
+    /**
+     * 删除
+     */
+    int deleteById(Integer id);
+
+    /**
+     * 修改
+     */
+    int updateById(Type type);
+
+    /**
+     * 根据ID查询
+     */
+    Type selectById(Integer id);
+
+    /**
+     * 查询所有
+     */
+    List<Type> selectAll(Type type);
+
+}
